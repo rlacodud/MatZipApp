7932c3397d825391e2f5796a65c85d1d800362da
rlacodud <rlacodud0313@naver.com>
2024-04-27 19:00:30 +0900

    feat: 로그인 화면 input 적용

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import InputFiled from '../../components/InputFiled';
 
 interface LoginScreenProps {
 
 }
 
 function LoginScreen({}: LoginScreenProps) {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeText = (name: string, text: string) => {
+    setValues({
+      ...values,
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-        <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputFiled
+          placeholder='이메일'
+          error={'이메일을 입력하세요'}
+          touched={touched.email}
+          inputMode='email'
+          value={values.email}
+          onChangeText={(text) => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputFiled
+          placeholder='비밀번호'
+          error={'비밀번호를 입력하세요'}
+          touched={touched.password}
+          secureTextEntry
+          value={values.password}
+          onChangeText={(text) => handleChangeText('password', text)}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  }
 });
 
 export default LoginScreen;
