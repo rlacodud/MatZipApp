848ef0a8674958d4dc1e2170952cd37fdd552102
rlacodud <rlacodud0313@naver.com>
2024-04-28 22:04:18 +0900

    build: server 세팅

--- front/src/components/CustomButton.tsx
+++ front/src/components/CustomButton.tsx
@@ -3,30 +3,30 @@ import {Pressable, StyleSheet, Text, PressableProps, Dimensions, View} from 'rea
 import { colors } from '../constants';
 
 interface CustomButtonProps extends PressableProps {
-    label: string;
-    varient?: 'filled' | 'outlined';
-    size?: 'large' | 'medium';
-    inValid?: boolean;
+  label: string;
+  varient?: 'filled' | 'outlined';
+  size?: 'large' | 'medium';
+  inValid?: boolean;
 }
 
 const deviceHeight = Dimensions.get('screen').height;
 
 function CustomButton({
-    label,
-    varient = 'filled',
-    size = 'large',
-    inValid = false,
-    ...props
+  label,
+  varient = 'filled',
+  size = 'large',
+  inValid = false,
+  ...props
 }: CustomButtonProps) {
   return (
     <Pressable
-        disabled={inValid}
-        style={({pressed}) => [
-            styles.container,
-            pressed ? styles[`${varient}Pressed`] : styles[varient],
-            inValid && styles.inValid
-        ]}
-        {...props}
+      disabled={inValid}
+      style={({pressed}) => [
+        styles.container,
+        pressed ? styles[`${varient}Pressed`] : styles[varient],
+        inValid && styles.inValid
+      ]}
+      {...props}
     >
       <View style={styles[size]}>
         <Text style={[styles.text, styles[`${varient}Text`]]}>{label}</Text>
@@ -36,53 +36,53 @@ function CustomButton({
 }
 
 const styles = StyleSheet.create({
-    container: {
-        borderRadius: 3,
-        justifyContent: 'center',
-        flexDirection: 'row',
-    },
-    inValid: {
-        opacity: 0.5,
-    },
-    filled: {
-        backgroundColor: colors.PINK_700,
-    },
-    outlined: {
-        borderColor: colors.PINK_700,
-        borderWidth: 1,
-    },
-    filledPressed: {
-      backgroundColor: colors.PINK_500,
-    },
-    outlinedPressed: {
-      borderColor: colors.PINK_700,
-      borderWidth: 1,
-      opacity: 0.5,
-    },
-    large: {
-        width: '100%',
-        paddingVertical: deviceHeight > 700 ? 15 : 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-    },
-    medium: {
-        width: '50%',
-        paddingVertical: deviceHeight > 700 ? 12 : 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-    },
-    text: {
-        fontSize: 16,
-        fontWeight: '700',
-    },
-    filledText: {
-        color: colors.WHITE,
-    },
-    outlinedText: {
-        color: colors.PINK_700,
-    },
+  container: {
+    borderRadius: 3,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  inValid: {
+    opacity: 0.5,
+  },
+  filled: {
+    backgroundColor: colors.PINK_700,
+  },
+  outlined: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+  },
+  filledPressed: {
+    backgroundColor: colors.PINK_500,
+  },
+  outlinedPressed: {
+    borderColor: colors.PINK_700,
+    borderWidth: 1,
+    opacity: 0.5,
+  },
+  large: {
+    width: '100%',
+    paddingVertical: deviceHeight > 700 ? 15 : 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  medium: {
+    width: '50%',
+    paddingVertical: deviceHeight > 700 ? 12 : 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  filledText: {
+    color: colors.WHITE,
+  },
+  outlinedText: {
+    color: colors.PINK_700,
+  },
 });
 
 export default CustomButton;
