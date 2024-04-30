a3fb85201f3745ce2ade3095f17c450d8a4cc8e9
rlacodud <rlacodud0313@naver.com>
2024-04-30 22:06:02 +0900

    feat: useLogin 초기 작업

--- front/src/hooks/queries/useAuth.ts
+++ front/src/hooks/queries/useAuth.ts
@@ -1,10 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
-import { postSignup } from "../../api/auth";
+import { postLogin, postSignup } from "../../api/auth";
 import { UseMutationCustomOptions } from "../../types/common";
+import { setEncryptStorage } from "../../utils";
 
 function useSignup(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: postSignup,
     ...mutationOptions,
   });
+}
+
+function useLogin() {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: ({accessToken, refreshToken}) => {
+      setEncryptStorage('refreshToken', refreshToken);
+    }
+  })
 }
