a3834d328a3b1476db63e2570881296514461bf9
rlacodud <rlacodud0313@naver.com>
2024-05-11 16:59:13 +0900

    feat: GRAY_100 color 추가

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -4,6 +4,7 @@ const colors = {
     RED_300: '#FFB4B4',
     RED_500: '#FF5F5F',
     WHITE: '#fff',
+    GRAY_100: '#F8F8F8',
     GRAY_200: '#E7E7E7',
     GRAY_500: '#8E8E8E',
     GRAY_700: '#575757',
