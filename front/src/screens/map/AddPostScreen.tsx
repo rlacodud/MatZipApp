086532b8f554dd7cab44fb099a48886449fe6e8e
rlacodud <rlacodud0313@naver.com>
2024-05-13 22:19:38 +0900

    feat: 장소 추가 screen 초기 작업

---
+++ front/src/screens/map/AddPostScreen.tsx
@@ -0,0 +1,22 @@
+import { mapNavigations } from '@/constants';
+import { MapStackParamList } from '@/navigations/stack/MapStackNavigator';
+import { StackScreenProps } from '@react-navigation/stack';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type AddPostScreenProps = StackScreenProps<
+  MapStackParamList,
+  typeof mapNavigations.ADD_POST
+>
+
+function AddPostScreen({route}: AddPostScreenProps) {
+  const {location} = route.params;
+
+  return (
+    <View><Text>{location.latitude}</Text></View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default AddPostScreen;
