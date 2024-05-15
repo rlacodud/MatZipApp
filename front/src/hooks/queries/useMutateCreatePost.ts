f641b99eb934b0293b85b699b536329268a0b0f1
rlacodud <rlacodud0313@naver.com>
2024-05-15 19:08:24 +0900

    feat: useMutateCreatePost 캐시 업데이트

--- front/src/hooks/queries/useMutateCreatePost.ts
+++ front/src/hooks/queries/useMutateCreatePost.ts
@@ -1,10 +1,36 @@
 import { createPost } from "@/api";
+import { queryClient } from "@/api/queryClient";
+import { colors, queryKeys } from "@/constants";
 import { UseMutationCustomOptions } from "@/types/common";
+import { Marker } from "@/types/domain";
 import { useMutation } from "@tanstack/react-query";
 
 function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
+    onSuccess:(newPost) => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [
+      //     queryKeys.MARKER, 
+      //     queryKeys.GET_MARKERS
+      // ]});
+
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            colors: newPost.color,
+            score: newPost.score,
+          }
+          return existingMarkers 
+          ? [...existingMarkers, newMarker]
+          : [newMarker];
+        }
+      )
+    },
     ...mutationOptions,
   });
 }
