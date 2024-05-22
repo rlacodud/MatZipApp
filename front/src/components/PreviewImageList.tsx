e0ebe0eee07d7c87187629351aa046b2e25466b5
rlacodud <rlacodud0313@naver.com>
2024-05-22 22:55:27 +0900

    feat: PreviewImageList 이미지 삭제/순서 변경 마크업 및 기능 적용

--- front/src/components/PreviewImageList.tsx
+++ front/src/components/PreviewImageList.tsx
@@ -1,21 +1,24 @@
+import { colors } from '@/constants';
 import { ImageUri } from '@/types/domain';
 import React from 'react';
 import {Image, Platform, Pressable, StyleSheet, View} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 interface PreviewImageListProps {
   imageUris: ImageUri[];
+  onDelete?: (uri:string) => void;
+  onChangeOrder: (fromIndex: number, toIndex: number) => void;
 }
 
-function PreviewImageList({imageUris}: PreviewImageListProps) {
+function PreviewImageList({imageUris, onDelete, onChangeOrder}: PreviewImageListProps) {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       <View style={styles.container}>
         {imageUris.map(({uri}, index) => {
           return (
-            <Pressable style={styles.imageContainer}>
+            <Pressable key={index} style={styles.imageContainer}>
               <Image 
-                key={index}
                 resizeMode='cover'
                 source={{
                   uri: `${Platform.OS === 'ios'
@@ -25,6 +28,26 @@ function PreviewImageList({imageUris}: PreviewImageListProps) {
                 }}
                 style={styles.image}
               />
+              <Pressable
+                style={[styles.imageButton, styles.deleteButton]}
+                onPress={() => onDelete && onDelete(uri)}>
+                <Ionicons name='close' size={16} color={colors.WHITE}/>
+              </Pressable>
+
+              {index > 0 && (
+                <Pressable
+                  style={[styles.imageButton, styles.moveLeftButton]}
+                  onPress={() => onChangeOrder && onChangeOrder(index, index - 1)}>
+                  <Ionicons name='arrow-back-outline' size={16} color={colors.WHITE}/>
+                </Pressable>
+              )}
+              {index < imageUris.length - 1 && (
+                <Pressable
+                  style={[styles.imageButton, styles.moveRightButton]}
+                  onPress={() => onChangeOrder && onChangeOrder(index, index + 1)}>
+                  <Ionicons name='arrow-forward-outline' size={16} color={colors.WHITE}/>
+                </Pressable>
+              )}
             </Pressable>
           )
         })}
@@ -48,6 +71,23 @@ const styles = StyleSheet.create({
   image: {
     width: '100%', 
     height: '100%',
+  },
+  imageButton: {
+    position: 'absolute',
+    backgroundColor: colors.BLACK,
+    zIndex: 1,
+  },
+  deleteButton: {
+    top: 0,
+    right: 0,
+  },
+  moveLeftButton: {
+    bottom: 0,
+    left: 0,
+  },
+  moveRightButton: {
+    bottom: 0,
+    right: 0,
   }
 });
 
