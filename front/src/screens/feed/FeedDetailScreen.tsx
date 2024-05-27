fcfe9b3ade35498ba58ca024232faa2c123e9676
rlacodud <rlacodud0313@naver.com>
2024-05-27 21:42:39 +0900

    feat: FeedDetailScreen  bottomTab 작업

--- front/src/screens/feed/FeedDetailScreen.tsx
+++ front/src/screens/feed/FeedDetailScreen.tsx
@@ -3,12 +3,14 @@ import useGetPost from '@/hooks/queries/useGetPost';
 import { FeedStackParamList } from '@/navigations/stack/FeedStackNavigator';
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import {Dimensions, Image, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Image, Platform, Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { getDateLocaleFormat } from '@/utils';
 import PreviewImageList from '@/components/common/PreviewImageList';
+import CustomButton from '@/components/common/CustomButton';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type FeedDetailScreenProps = StackScreenProps<
   FeedStackParamList,
@@ -18,87 +20,104 @@ type FeedDetailScreenProps = StackScreenProps<
 function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
   const {id} = route.params;
   const {data: post, isPending, isError} = useGetPost(id);
+  const insets = useSafeAreaInsets();
 
   if(isPending || isError) {
     return <></>
   }
   return (
-    <ScrollView style={styles.container}>
-      <SafeAreaView style={styles.headerContainer}>
-        <View style={styles.header}>
-          <Octicons 
-            name='arrow-left' 
-            size={30} 
-            color={colors.WHITE}
-            onPress={() => navigation.goBack()}
-          />
-          <Ionicons name='ellipsis-vertical' size={30} color={colors.WHITE}/>
-        </View>
-      </SafeAreaView>
-      <View style={styles.imageContainer}>
-        {post?.images.length > 0 && (
-          <Image 
-            style={styles.image}
-            source={{
-              uri: `${Platform.OS === 'ios'
-              ? `http://localhost:3030`
-              : `http"//10.0.2.2:3030`
-              }/${post?.images[0]?.uri}`
-              }}
-            resizeMode='cover'
-          />
-        )}
-        {post?.images.length === 0 && (
-          <View style={styles.emptyImageContainer}>
-            <Text>No Image</Text>
+    <>
+      <ScrollView style={styles.container}>
+        <SafeAreaView style={styles.headerContainer}>
+          <View style={styles.header}>
+            <Octicons 
+              name='arrow-left' 
+              size={30} 
+              color={colors.WHITE}
+              onPress={() => navigation.goBack()}
+            />
+            <Ionicons name='ellipsis-vertical' size={30} color={colors.WHITE}/>
           </View>
-        )}
-      </View>
+        </SafeAreaView>
+        <View style={styles.imageContainer}>
+          {post?.images.length > 0 && (
+            <Image 
+              style={styles.image}
+              source={{
+                uri: `${Platform.OS === 'ios'
+                ? `http://localhost:3030`
+                : `http"//10.0.2.2:3030`
+                }/${post?.images[0]?.uri}`
+                }}
+              resizeMode='cover'
+            />
+          )}
+          {post?.images.length === 0 && (
+            <View style={styles.emptyImageContainer}>
+              <Text>No Image</Text>
+            </View>
+          )}
+        </View>
 
-      <View style={styles.contentsContainer}>
-        <View style={styles.addressContainer}>
-          <Octicons name='location' size={10} color={colors.GRAY_500}/>
-          <Text 
-            style={styles.addressText} 
-            ellipsizeMode='tail'
-            numberOfLines={1}
-          >
-            {post.address}
-          </Text>
-        </View>
-        <Text style={styles.titleText}>{post.title}</Text>
-        <View style={styles.infoContainer}>
-          <View style={styles.infoRow}>
-            <View style={styles.infoColumn}>
-              <Text style={styles.infoColumnKeyText}>방문날짜</Text>
-              <Text style={styles.infoColumnValueText}>{getDateLocaleFormat(post.date)}</Text>
+        <View style={styles.contentsContainer}>
+          <View style={styles.addressContainer}>
+            <Octicons name='location' size={10} color={colors.GRAY_500}/>
+            <Text 
+              style={styles.addressText} 
+              ellipsizeMode='tail'
+              numberOfLines={1}
+            >
+              {post.address}
+            </Text>
+          </View>
+          <Text style={styles.titleText}>{post.title}</Text>
+          <View style={styles.infoContainer}>
+            <View style={styles.infoRow}>
+              <View style={styles.infoColumn}>
+                <Text style={styles.infoColumnKeyText}>방문날짜</Text>
+                <Text style={styles.infoColumnValueText}>{getDateLocaleFormat(post.date)}</Text>
+              </View>
+              <View style={styles.infoColumn}>
+                <Text style={styles.infoColumnKeyText}>평점</Text>
+                <Text style={styles.infoColumnValueText}>{post.score}점</Text>
+              </View>
             </View>
-            <View style={styles.infoColumn}>
-              <Text style={styles.infoColumnKeyText}>평점</Text>
-              <Text style={styles.infoColumnValueText}>{post.score}점</Text>
+            <View style={styles.infoRow}>
+              <View style={styles.infoColumn}>
+                <Text style={styles.infoColumnKeyText}>마커색상</Text>
+                <View 
+                  style={[
+                    styles.markerColor, 
+                    {backgroundColor: colorHex[post.color]}
+                  ]}
+                />
+              </View>
             </View>
           </View>
-          <View style={styles.infoRow}>
-            <View style={styles.infoColumn}>
-              <Text style={styles.infoColumnKeyText}>마커색상</Text>
-              <View 
-                style={[
-                  styles.markerColor, 
-                  {backgroundColor: colorHex[post.color]}
-                ]}
-              />
-            </View>
-          </View>
+          <Text style={styles.descriptionText}>{post.description}</Text>
         </View>
-        <Text style={styles.descriptionText}>{post.description}</Text>
-      </View>
 
-      {post.images.length > 0 && (
-        <View style={styles.imageContentsContainer}>
-          <PreviewImageList imageUris={post.images} />
+        {post.images.length > 0 && (
+          <View style={styles.imageContentsContainer}>
+            <PreviewImageList imageUris={post.images} />
+          </View>
+        )}
+      </ScrollView>
+
+      <View style={[styles.bottomContainer, {paddingBottom: insets.bottom}]}>
+        <View style={[styles.tabContainer, insets.bottom === 0 && styles.tabContainerNoInsets]}>
+          <Pressable>
+            <Octicons name='star-fill' size={30} color={colors.GRAY_100}/>
+          </Pressable>
+          <CustomButton
+            label='위치보기'
+            size='medium'
+            varient='filled'
+            onPress={() => {}}
+          />
         </View>
-      )}
-    </ScrollView>
+      </View>
+    </>
   )
 }
 
@@ -187,6 +206,25 @@ const styles = StyleSheet.create({
   imageContentsContainer: {
     paddingVertical: 15,
     backgroundColor: colors.WHITE,
+    marginBottom: 10,
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    alignItems: 'flex-end',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    backgroundColor: colors.WHITE,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.GRAY_200,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  tabContainerNoInsets: {
     marginBottom: 10,
   }
 });
