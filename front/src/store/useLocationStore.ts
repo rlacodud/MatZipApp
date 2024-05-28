ed0785b6afd09cb289fca0b1ece97ae5adf61357
rlacodud <rlacodud0313@naver.com>
2024-05-28 23:58:14 +0900

    feat: useLocationStore store 생성

---
+++ front/src/store/useLocationStore.ts
@@ -0,0 +1,15 @@
+import { LatLng } from "react-native-maps";
+import { create } from "zustand";
+
+interface LocationState {
+  moveLocation: LatLng | null;
+  setMoveLocation: (location: LatLng) => void;
+}
+const useLocationStore = create<LocationState>(set =>( {
+  moveLocation: null,
+  setMoveLocation: (moveLocation: LatLng) => {
+    set({moveLocation});
+  }
+}))
+
+export default useLocationStore;
