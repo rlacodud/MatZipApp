6e226faa133b03dcdd4ac91e8758b9eeb8d693a2
rlacodud <rlacodud0313@naver.com>
2024-06-01 15:46:17 +0900

    feat: FeedDetailOption 장소 삭제 기능 구현

--- front/src/components/feed/FeedDetailOption.tsx
+++ front/src/components/feed/FeedDetailOption.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Alert, StyleSheet} from 'react-native';
 import { CompoundOption } from '../common/CompoundOption';
+import useMutateDeletePost from '@/hooks/queries/useMutateDeletePost';
+import useDetailStore from '@/store/useDetailPostStore';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { FeedStackParamList } from '@/navigations/stack/FeedStackNavigator';
+import { alerts } from '@/constants';
 
 interface FeedDetailOptionProps {
   isVisible: boolean;
@@ -8,11 +14,39 @@ interface FeedDetailOptionProps {
 }
 
 function FeedDetailOption({isVisible, hideOption}: FeedDetailOptionProps) {
+  const navigation = useNavigation<StackNavigationProp<FeedStackParamList>>();
+  const deletePost = useMutateDeletePost();
+  const {detailPost} = useDetailStore();
+
+  const handleDeletePost = () => {
+    if(!detailPost) {
+      return;
+    }
+
+    Alert.alert(alerts.DELETE_POST.TITLE, alerts.DELETE_POST.DESCRIPTION, [
+      {
+        text: '삭제',
+        onPress: () => {
+          deletePost.mutate(detailPost.id, {
+            onSuccess: () => {
+              hideOption();
+              navigation.goBack();
+            }
+          });
+        },
+        style: 'destructive',
+      },{
+        text: '취소',
+        style: 'cancel',
+      }
+    ])
+  }
+
   return (
     <CompoundOption isVisible={isVisible} hideOption={hideOption}>
       <CompoundOption.Background>
         <CompoundOption.Container>
-              <CompoundOption.Button isDanger>삭제하기</CompoundOption.Button>
+              <CompoundOption.Button isDanger onPress={handleDeletePost}>삭제하기</CompoundOption.Button>
               <CompoundOption.Divider/>
               <CompoundOption.Button>수정하기</CompoundOption.Button>
           </CompoundOption.Container>
