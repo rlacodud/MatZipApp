602b3b7ae57af49753d7dc557f0ff33d12164de8
rlacodud <rlacodud0313@naver.com>
2024-06-02 20:43:07 +0900

    chore: FeedStackNavigator 장소 수정 screen 추가

--- front/src/navigations/stack/FeedStackNavigator.tsx
+++ front/src/navigations/stack/FeedStackNavigator.tsx
@@ -6,10 +6,13 @@ import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
 import FeedHomeHeaderLeft from '@/components/feed/FeedHomeHeaderLeft';
 import { colors } from '@/constants';
 import FeedDetailScreen from '@/screens/feed/FeedDetailScreen';
+import { LatLng } from 'react-native-maps';
+import EditPostScreen from '@/screens/feed/EditPostScreen';
 
 export type FeedStackParamList = {
   [feedNavigations.FEED_HOME]: undefined;
   [feedNavigations.FEED_DETAIL]: {id: number};
+  [feedNavigations.EDIT_POST]: {location: LatLng};
 }
 
 const Stack = createStackNavigator<FeedStackParamList>();
@@ -47,6 +50,13 @@ function FeedStackNavigator() {
         }}
         name={feedNavigations.FEED_DETAIL} 
         component={FeedDetailScreen} 
+      />
+      <Stack.Screen
+        name={feedNavigations.EDIT_POST}
+        component={EditPostScreen}
+        options={{
+          headerTitle: '장소 수정'
+        }}
       />
     </Stack.Navigator>
   )
