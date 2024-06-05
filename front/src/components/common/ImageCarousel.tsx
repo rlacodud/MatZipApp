7a3ec5d671c900d6d8326ab2f21d3a3e6ea94358
rlacodud <rlacodud0313@naver.com>
2024-06-05 21:31:00 +0900

    feat: ImageCarousel 뒤로가기 버튼 구현

--- front/src/components/common/ImageCarousel.tsx
+++ front/src/components/common/ImageCarousel.tsx
@@ -1,7 +1,10 @@
 import { colors } from '@/constants';
 import { ImageUri } from '@/types/domain';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {Dimensions, FlatList, Image, Platform, StyleSheet, View} from 'react-native';
+import {Dimensions, FlatList, Image, Platform, Pressable, StyleSheet, View} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 interface ImageCarouselProps {
   images: ImageUri[];
@@ -10,8 +13,16 @@ interface ImageCarouselProps {
 const deviceWidth = Dimensions.get('window').width;
 
 function ImageCarousel({images}: ImageCarouselProps) {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
+      <Pressable 
+        style={[styles.backButton, {marginTop: insets.top + 10}]}
+        onPress={() => navigation.goBack()}
+      >
+        <Octicons name='arrow-left' size={30} color={colors.WHITE}/>
+      </Pressable>
       <FlatList
         data={images}
         renderItem={({item}) => (
@@ -40,6 +51,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: colors.WHITE,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+    zIndex: 1,
+    backgroundColor: colors.PINK_700,
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
     width: '100%',
