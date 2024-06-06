cc94fabc908a53051f8d19729de5eda949ac3242
rlacodud <rlacodud0313@naver.com>
2024-06-06 22:59:46 +0900

    feat: ImageCarousel 페이지 인디게이터 구현

--- front/src/components/common/ImageCarousel.tsx
+++ front/src/components/common/ImageCarousel.tsx
@@ -2,7 +2,7 @@ import { colors } from '@/constants';
 import { ImageUri } from '@/types/domain';
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
-import {Dimensions, FlatList, Image, Platform, Pressable, StyleSheet, View} from 'react-native';
+import {Dimensions, FlatList, Image, NativeScrollEvent, NativeSyntheticEvent, Platform, Pressable, StyleSheet, View} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Octicons from 'react-native-vector-icons/Octicons';
 
@@ -16,7 +16,14 @@ const deviceWidth = Dimensions.get('window').width;
 function ImageCarousel({images, pressedindex = 0}: ImageCarouselProps) {
   const insets = useSafeAreaInsets();
   const navigation = useNavigation();
+  const [page, setPage] = useState(pressedindex);
   const [initialIndex, setInitialIndex] = useState(pressedindex);
+
+  const handleScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const newPage = Math.round(e.nativeEvent.contentOffset.x / deviceWidth);
+
+    setPage(newPage);
+  }
 
   return (
     <View style={styles.container}>
@@ -41,6 +48,7 @@ function ImageCarousel({images, pressedindex = 0}: ImageCarouselProps) {
           </View>
         )}
         keyExtractor={item => String(item.id)}
+        onScroll={handleScroll}
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
@@ -48,7 +56,17 @@ function ImageCarousel({images, pressedindex = 0}: ImageCarouselProps) {
         onScrollToIndexFailed={() => {
           setInitialIndex(0);
         }}
+        getItemLayout={(_, index) => ({
+          length: deviceWidth,
+          offset: deviceWidth * index,
+          index,
+        })}
       />
+      <View style={[styles.pageContainer, {bottom: insets.bottom + 10}]}>
+        {Array.from({length: images.length}, (_, index) => (
+          <View key={index} style={[styles.pageDot, index === page && styles.currentPageDot]}/>
+        ))}
+      </View>
     </View>
   )
 }
@@ -73,6 +91,21 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  pageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  pageDot: {
+    margin: 4,
+    backgroundColor: colors.GRAY_200,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+  },
+  currentPageDot: {
+    backgroundColor: colors.PINK_700
   }
 });
 
