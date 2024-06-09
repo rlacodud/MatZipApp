d0ba36910bb015adcd6196d4fd1953d19c6fcee7
rlacodud <rlacodud0313@naver.com>
2024-06-09 21:44:19 +0900

    feat: Calendar components 생성 및 초기 작업
    - 연도 버튼 마크업

---
+++ front/src/components/calendar/Calendar.tsx
@@ -0,0 +1,49 @@
+import { colors } from '@/constants';
+import React from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface CalendarProps {
+
+}
+
+function Calendar({}: CalendarProps) {
+  return (
+    <View style={styles.headerContainer}>
+      <Pressable style={styles.monthButtonContainer}>
+        <Ionicons name='arrow-back' size={25} color={colors.BLACK}/>
+      </Pressable>
+      <Pressable style={styles.monthYearContainer}>
+        <Text style={styles.titleText}>2024년 10월</Text>
+      </Pressable>
+      <Pressable style={styles.monthButtonContainer}>
+        <Ionicons name='arrow-forward' size={25} color={colors.BLACK} />
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 25,
+    marginVertical: 16,
+  },
+  monthYearContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  monthButtonContainer: {
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: colors.BLACK,
+  }
+});
+
+export default Calendar;
