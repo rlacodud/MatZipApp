cbf84c83021e3d260b571d5b88108826ae796425
rlacodud <rlacodud0313@naver.com>
2024-06-09 21:44:40 +0900

    chore: CalendarHomeScreen Calendar component 적용

--- front/src/screens/calendar/CalendarHomeScreen.tsx
+++ front/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,5 +1,7 @@
+import Calendar from '@/components/calendar/Calendar';
+import { colors } from '@/constants';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
 interface CalendarHomeScreenProps {
 
@@ -7,10 +9,17 @@ interface CalendarHomeScreenProps {
 
 function CalendarHomeScreen({}: CalendarHomeScreenProps) {
   return (
-    <View></View>
+    <SafeAreaView style={styles.container}>
+      <Calendar/>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  }
+});
 
 export default CalendarHomeScreen;
