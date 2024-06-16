13c7a63fb8491b1830a73a72ffc2464a38579b9b
rlacodud <rlacodud0313@naver.com>
2024-06-16 16:08:16 +0900

    feat: EventList 클릭 시 상세 페이지로 이동 구현

--- front/src/components/calendar/EventList.tsx
+++ front/src/components/calendar/EventList.tsx
@@ -1,5 +1,10 @@
 import { CalendarPost } from '@/api';
-import { colors } from '@/constants';
+import { colors, feedNavigations, feedTabNavigations, mainNavigations, mapNavigations } from '@/constants';
+import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
+import { FeedTabParamList } from '@/navigations/tab/FeedTabNavigator';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
 import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -9,8 +14,24 @@ interface EventListProps {
   posts: CalendarPost[];
 }
 
+type Navigation = CompositeNavigationProp<
+DrawerNavigationProp<MainDrawerParamList>,
+BottomTabNavigationProp<FeedTabParamList>
+>
 function EventList({posts}: EventListProps) {
+  const navigation = useNavigation<Navigation>();
   const insets = useSafeAreaInsets();
+
+  const handlePressItem = (id: number) => {
+    navigation.navigate(mainNavigations.FEED, {
+      screen: feedTabNavigations.FEED_HOME,
+      params: {
+        screen: feedNavigations.FEED_DETAIL,
+        params: {id},
+        initial: false,
+      }
+    })
+  }
 
   return (
     <ScrollView 
@@ -22,7 +43,11 @@ function EventList({posts}: EventListProps) {
         {marginBottom: insets.bottom + 30}
       ]}>
         {posts?.map(post => (
-          <Pressable key={post.id} style={styles.itemContainer}>
+          <Pressable 
+            key={post.id} 
+            style={styles.itemContainer}
+            onPress={() => handlePressItem(post.id)}
+          >
             <View style={styles.itemHeader}></View>
             <View style={styles.infoContainer}>
               <Text 
