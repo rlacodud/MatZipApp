8090d6ceb390c46646fd0c2301c022c984dd8897
rlacodud <rlacodud0313@naver.com>
2024-06-17 22:38:33 +0900

    feat: numbers 상수 추가
    - MIN_CALENDAR_YEAR
    - MAX_CALENDAR_YEAR
    -  CALENDAR_YEAR_SELECTOR_COLUMN

--- front/src/constants/numbers.ts
+++ front/src/constants/numbers.ts
@@ -3,7 +3,10 @@ const numbers = {
   INITIAL_DELTA: {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
-  }
+  },
+  MIN_CALENDAR_YEAR: 1980,
+  MAX_CALENDAR_YEAR: 2099,
+  CALENDAR_YEAR_SELECTOR_COLUMN: 4,
 } as const;
 
 export {numbers};
