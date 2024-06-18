cca6509b2720fae671cedbaff0b24c4d17825eec
rlacodud <rlacodud0313@naver.com>
2024-06-18 17:44:35 +0900

    feat: CalendarHomeHeaderRight component 생성

---
+++ front/src/components/calendar/CalendarHomeHeaderRight.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderButton from '../common/HeaderButton';
+
+function CalendarHomeHeaderRight(onPress: () => void) {
+  return (
+    <HeaderButton labelText='오늘' onPress={onPress}/>
+  )
+}
+
+export default CalendarHomeHeaderRight;
