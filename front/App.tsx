f913090a64a4b7753c225ba35c148af5ab9fc655
rlacodud <rlacodud0313@naver.com>
2024-06-22 17:01:57 +0900

    chore: Toast 커스텀

--- front/App.tsx
+++ front/App.tsx
@@ -4,7 +4,36 @@ import { StyleSheet } from 'react-native';
 import RootNavigator from './src/navigations/root/RootNavigator';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './src/api/queryClient';
-import Toast from 'react-native-toast-message';
+import Toast, { BaseToast, BaseToastProps, ErrorToast } from 'react-native-toast-message';
+import { colors } from '@/constants';
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: colors.BLUE_500 }}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{ borderLeftColor: colors.RED_500 }}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 function App(): React.JSX.Element {
 
@@ -12,7 +41,7 @@ function App(): React.JSX.Element {
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator/>
-        <Toast/>
+        <Toast config={toastConfig}/>
       </NavigationContainer>
     </QueryClientProvider>
   );
