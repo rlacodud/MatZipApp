34e1b5c43be2347672f16bfa913cade4947a3e88
rlacodud <rlacodud0313@naver.com>
2024-06-29 17:18:57 +0900

    chore: SearchLocationScreen SearchInput 적용

--- front/src/screens/map/SearchLocationScreen.tsx
+++ front/src/screens/map/SearchLocationScreen.tsx
@@ -1,28 +1,36 @@
+import SearchInput from '@/components/common/SearchInput';
 import useSearchLocation from '@/hooks/useSearchLocation';
 import useUserLocation from '@/hooks/useUserLocation';
 import React, { useState } from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Keyboard, StyleSheet, View} from 'react-native';
 
 function SearchLocationScreen() {
-	const [keyword, setKeyword] = useState<string>('');
-	const {userLocation} = useUserLocation();
-	useSearchLocation(keyword, userLocation);
+    const [keyword, setKeyword] = useState<string>('');
+    const {userLocation} = useUserLocation();
+    const {regionInfo} = useSearchLocation(keyword, userLocation);
 
-	const handleChangeKeyword = (text: string) => {
-		setKeyword(text);
-	};
+    const handleChangeKeyword = (text: string) => {
+        setKeyword(text);
+    };
 
-	return (
-		<View style={styles.container}>
-		</View>
-	)
+    return (
+        <View style={styles.container}>
+            <SearchInput
+                autoFocus
+                value={keyword}
+        onChangeText={handleChangeKeyword}
+        placeholder='검색할 장소를 입력하세요.'
+        onSubmit={() => Keyboard.dismiss()}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 20,
-	},
+    container: {
+        flex: 1,
+        padding: 20,
+    },
 });
 
 export default SearchLocationScreen;
