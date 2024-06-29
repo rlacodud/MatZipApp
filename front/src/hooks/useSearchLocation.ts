4ff6e6e1b0119519fdcae228df36a43fcfd29c78
rlacodud <rlacodud0313@naver.com>
2024-06-29 17:19:41 +0900

    feat: useSearchLocation type 작성 및 setRegionInfo

--- front/src/hooks/useSearchLocation.ts
+++ front/src/hooks/useSearchLocation.ts
@@ -3,26 +3,59 @@ import { useEffect, useState } from "react";
 import Config from "react-native-config";
 import { LatLng } from "react-native-maps";
 
+type Meta = {
+  total_count: number;
+  pageable_count: number;
+  is_end: boolean;
+  same_name: {
+    region: string[];
+    keyword: string;
+    selected_region: string;
+  };
+};
+
+export type RegionInfo = {
+  address_name: string;
+  category_group_code: string;
+  category_group_name: string;
+  category_name: string;
+  distance: string;
+  id: string;
+  phone: string;
+  place_name: string;
+  place_url: string;
+  road_address_name: string;
+  x: string;
+  y: string;
+};
+
+type RegionResponse = {
+  meta: Meta;
+  documents: RegionInfo[];
+};
+
 function useSearchLocation(keyword: string, location: LatLng) {
+  const [regionInfo, setRegionInfo] = useState<RegionInfo[]>([]);
   const [pageParam, setPageParam] = useState(1);
 
   useEffect(() => {
     (async () => {
       try {
-        const {data} = await axios.get(
+        const {data} = await axios.get<RegionResponse>(
           `https://dapi.kakao.com/v2/local/search/category.json?query=${keyword}&y=${location.latitude}&x=${location.longitude}&page=${pageParam}`,
           {
             headers: {
-              Authorization: `kakaoAK ${Config.KAKAO_REST_API_KEY}`,
+              Authorization: `KakaoAK ${Config.KAKAO_REST_API_KEY}`,
             },
           },
         );
-        console.log(data);
+        setRegionInfo(data.documents);
       } catch (error) {
-
+        setRegionInfo([]);
       }
-    })
-  })
+    })();
+  }, [keyword, location]);
+  return {regionInfo};
 }
 
 export default useSearchLocation;
