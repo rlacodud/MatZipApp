7282a0d0b03c58a783b16dc1150b5e401e177059
rlacodud <rlacodud0313@naver.com>
2024-06-30 16:12:13 +0900

    chore: SearchLocationScreen SearchRegionInfo 적용

--- front/src/screens/map/SearchLocationScreen.tsx
+++ front/src/screens/map/SearchLocationScreen.tsx
@@ -1,36 +1,38 @@
 import SearchInput from '@/components/common/SearchInput';
+import SearchRegionInfo from '@/components/map/SearchRegionInfo';
 import useSearchLocation from '@/hooks/useSearchLocation';
 import useUserLocation from '@/hooks/useUserLocation';
 import React, { useState } from 'react';
 import {Keyboard, StyleSheet, View} from 'react-native';
 
 function SearchLocationScreen() {
-    const [keyword, setKeyword] = useState<string>('');
-    const {userLocation} = useUserLocation();
-    const {regionInfo} = useSearchLocation(keyword, userLocation);
+  const [keyword, setKeyword] = useState<string>('');
+  const {userLocation} = useUserLocation();
+  const {regionInfo} = useSearchLocation(keyword, userLocation);
 
-    const handleChangeKeyword = (text: string) => {
-        setKeyword(text);
-    };
+  const handleChangeKeyword = (text: string) => {
+    setKeyword(text);
+  };
 
-    return (
-        <View style={styles.container}>
-            <SearchInput
-                autoFocus
-                value={keyword}
+  return (
+    <View style={styles.container}>
+      <SearchInput
+        autoFocus
+        value={keyword}
         onChangeText={handleChangeKeyword}
         placeholder='검색할 장소를 입력하세요.'
         onSubmit={() => Keyboard.dismiss()}
-            />
-        </View>
-    )
+      />
+      <SearchRegionInfo regionInfo={regionInfo}/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
 });
 
 export default SearchLocationScreen;
