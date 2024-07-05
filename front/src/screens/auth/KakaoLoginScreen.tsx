871e56ee0e5b4c887defb8235ecab9084ef6edd6
rlacodud <rlacodud0313@naver.com>
2024-07-05 20:49:05 +0900

    feat: KakaoLoginScreen 웹뷰 연결

--- front/src/screens/auth/KakaoLoginScreen.tsx
+++ front/src/screens/auth/KakaoLoginScreen.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import Config from 'react-native-config';
+import WebView from 'react-native-webview';
 
 interface KakaoLoginScreenProps {
 
 }
 
+const REDIRECT_URI = 'http://localhost:3030/auth/oauth/kakao';
+
 function KakaoLoginScreen({}: KakaoLoginScreenProps) {
   return (
-    <View></View>
+    <SafeAreaView style={styles.container}>
+      <WebView source={{
+        uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Config.KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
+      }}/>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
 
 export default KakaoLoginScreen;
