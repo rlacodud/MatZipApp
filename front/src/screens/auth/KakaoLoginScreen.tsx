8720162739dbbef1b3e3259ac451a6fe0fd3d8ca
rlacodud <rlacodud0313@naver.com>
2024-07-06 17:43:57 +0900

    feat: KakaoLoginScreen 카카오 로그인 안드로이드 분기 처리

--- front/src/screens/auth/KakaoLoginScreen.tsx
+++ front/src/screens/auth/KakaoLoginScreen.tsx
@@ -1,20 +1,50 @@
+import useAuth from '@/hooks/queries/useAuth';
+import axios from 'axios';
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import Config from 'react-native-config';
-import WebView from 'react-native-webview';
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
 
-interface KakaoLoginScreenProps {
+const REDIRECT_URI = `${Platform.OS === 'ios'
+  ? 'http://localhost:3030/'
+  : 'http://10.0.2.2:3030/'
+  }auth/oauth/kakao`;
 
-}
+function KakaoLoginScreen() {
+  const {kakaoLoginMutation} = useAuth();
 
-const REDIRECT_URI = 'http://localhost:3030/auth/oauth/kakao';
+  const handleOnMessage = (event: WebViewMessageEvent) => {
+    if(event.nativeEvent.url.includes(`${REDIRECT_URI}?code=`)) {
+      const code = event.nativeEvent.url.replace(`${REDIRECT_URI}?code=`, '');
+  
+      requestToken(code);
+    }
+  };
+  
+  const requestToken = async(code: string) => {
+    const response = await axios({
+      method: 'post',
+      url: 'https://kauth.kakao.com/oauth/token',
+      params: {
+        grant_type: 'authorization_code',
+        client_id: Config.KAKAO_REST_API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code,
+      }
+    });
+  
+    console.log('response.data', response.data);
+    kakaoLoginMutation.mutate(response.data.access_token);
+  }
 
-function KakaoLoginScreen({}: KakaoLoginScreenProps) {
   return (
     <SafeAreaView style={styles.container}>
       <WebView source={{
         uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Config.KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
-      }}/>
+      }}
+      onMessage={handleOnMessage}
+      injectedJavaScript={"window.ReactNativeWebView.postMessage('')"}
+      />
     </SafeAreaView>
   )
 }
