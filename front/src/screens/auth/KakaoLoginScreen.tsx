b601fa3632a12655bb5bf08f56c2e63b6e67f259
rlacodud <rlacodud0313@naver.com>
2024-07-06 17:52:01 +0900

    update: KakaoLoginScreen Loading 처리

--- front/src/screens/auth/KakaoLoginScreen.tsx
+++ front/src/screens/auth/KakaoLoginScreen.tsx
@@ -1,9 +1,10 @@
+import { colors } from '@/constants';
 import useAuth from '@/hooks/queries/useAuth';
 import axios from 'axios';
-import React from 'react';
-import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, { useState } from 'react';
+import {ActivityIndicator, Dimensions, Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import Config from 'react-native-config';
-import WebView, { WebViewMessageEvent } from 'react-native-webview';
+import WebView, { WebViewMessageEvent, WebViewNavigation } from 'react-native-webview';
 
 const REDIRECT_URI = `${Platform.OS === 'ios'
   ? 'http://localhost:3030/'
@@ -12,6 +13,8 @@ const REDIRECT_URI = `${Platform.OS === 'ios'
 
 function KakaoLoginScreen() {
   const {kakaoLoginMutation} = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isChangeNavigate, setIsChangeNavigate] = useState(true);
 
   const handleOnMessage = (event: WebViewMessageEvent) => {
     if(event.nativeEvent.url.includes(`${REDIRECT_URI}?code=`)) {
@@ -33,17 +36,28 @@ function KakaoLoginScreen() {
       }
     });
   
-    console.log('response.data', response.data);
     kakaoLoginMutation.mutate(response.data.access_token);
+  }
+
+  const handleNavigationChangeState = (event: WebViewNavigation) => {
+    const isMatched = event.url.includes(`${REDIRECT_URI}?code=`);
+    setIsLoading(isMatched);
+    setIsChangeNavigate(event.loading);
   }
 
   return (
     <SafeAreaView style={styles.container}>
+      {(isLoading || isChangeNavigate) &&
+        <View style={styles.kakaoLoadingContainer}>
+          <ActivityIndicator size={'small'} color={colors.BLACK}/>
+        </View>
+      }
       <WebView source={{
         uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Config.KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
       }}
       onMessage={handleOnMessage}
       injectedJavaScript={"window.ReactNativeWebView.postMessage('')"}
+      onNavigationStateChange={handleNavigationChangeState}
       />
     </SafeAreaView>
   )
@@ -52,6 +66,13 @@ function KakaoLoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  kakaoLoadingContainer: {
+    backgroundColor: colors.WHITE,
+    height: Dimensions.get('window').height,
+    padding: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
