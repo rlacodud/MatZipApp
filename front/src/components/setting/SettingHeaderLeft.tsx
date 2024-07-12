7152982fabf3b152b6f7313e952d0ecbc47ce0ae
rlacodud <rlacodud0313@naver.com>
2024-07-12 20:34:50 +0900

    feat: SettingHeaderLeft component 생성

---
+++ front/src/components/setting/SettingHeaderLeft.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import HeaderButton from '../common/HeaderButton';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { colors } from '@/constants';
+import { CompositeNavigationProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { SettingStackParamList } from '@/navigations/stack/SettingStackNavigator';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { MainDrawerParamList } from '@/navigations/drawer/MainDrawerNavigator';
+
+type SettingHeaderLeftProps = CompositeNavigationProp<
+  StackNavigationProp<SettingStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
+
+function SettingHeaderLeft(navigation: SettingHeaderLeftProps) {
+  return (
+    <HeaderButton
+      icon={<Ionicons name='menu' color={colors.BLACK} size={25} />}
+      onPress={() => navigation.openDrawer()}
+    />
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default SettingHeaderLeft;
