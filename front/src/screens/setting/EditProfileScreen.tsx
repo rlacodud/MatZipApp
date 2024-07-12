4a1781b48f58ba8030fbdc4e9b6af03a169ec73b
rlacodud <rlacodud0313@naver.com>
2024-07-12 20:33:52 +0900

    feat: 설정 관련 스크린 초기 생성
    - SettingHomeScreen
    - EditProfileScreen

---
+++ front/src/screens/setting/EditProfileScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+interface EditProfileScreenProps {
+
+}
+
+function EditProfileScreen({}: EditProfileScreenProps) {
+  return (
+    <View></View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default EditProfileScreen;
