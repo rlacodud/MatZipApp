20d610a8f05b5a5c9b1d9128f27075eb6ea1c333
rlacodud <rlacodud0313@naver.com>
2024-07-15 20:06:54 +0900

    chore: SettingHomeScreen 기초 작업 및 로그아웃 기능 구현

--- front/src/screens/setting/SettingHomeScreen.tsx
+++ front/src/screens/setting/SettingHomeScreen.tsx
@@ -1,16 +1,57 @@
+import SettingItem from '@/components/setting/SettingItem';
+import { colors, settingNavigations } from '@/constants';
+import useAuth from '@/hooks/queries/useAuth';
+import { SettingStackParamList } from '@/navigations/stack/SettingStackNavigator';
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Octicons from 'react-native-vector-icons/Octicons';
 
-interface SettingHomeScreenProps {
+type SettingHomeScreenProps = StackScreenProps<SettingStackParamList>
 
-}
+function SettingHomeScreen({navigation}: SettingHomeScreenProps) {
+  const {logoutMutation} = useAuth();
 
-function SettingHomeScreen({}: SettingHomeScreenProps) {
+  const handlePressEditProfile = () => {
+    navigation.navigate(settingNavigations.EDIT_PROFILE);
+  };
+
+  const handlePressLogout = () => {
+    logoutMutation.mutate(null);
+  }
+
   return (
-    <View></View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.space}/>
+        <SettingItem title='프로필 수정' onPress={handlePressEditProfile} />
+        <SettingItem title='마커 카테고리 설정' />
+        <View style={styles.space}/>
+        <SettingItem 
+          title='로그아웃' 
+          color={colors.RED_500}
+          icon={
+            <Octicons
+              name='sign-out' 
+              color={colors.RED_500} 
+              size={16}
+            />
+          }
+          onPress={handlePressLogout}
+        />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  space: {
+    height: 30,
+  }
+});
 
 export default SettingHomeScreen;
