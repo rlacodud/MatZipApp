5552625ff234e90b3d05647373b1fd1f36e734ef
rlacodud <rlacodud0313@naver.com>
2024-07-21 14:39:11 +0900

    feat: EDIT_CATEGORY navigations 추가

--- front/src/constants/navigations.ts
+++ front/src/constants/navigations.ts
@@ -34,6 +34,7 @@ const settingNavigations = {
   SETTING_HOME: 'SettingHome',
   EDIT_PROFILE: 'EditProfile',
   DELETE_ACCOUNT: 'DeleteAccount',
+  EDIT_CATEGORY: 'EditCategory',
 } as const;
 
 export {mainNavigations, authNavigations, mapNavigations, feedNavigations, feedTabNavigations, settingNavigations};
