a58cfb5a54681e3f6a16319a4e3497acbe2afbc1
rlacodud <rlacodud0313@naver.com>
2024-07-23 20:51:03 +0900

    feat: DarkModeOption component 생성

---
+++ front/src/components/setting/DarkModeOption.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, View, useColorScheme} from 'react-native';
+import { CompoundOption } from '../common/CompoundOption';
+import useThemeStorage from '@/hooks/useThemeStorage';
+
+interface DarkModeOptionProps {
+  isVisible: boolean;
+  hideOption: () => void;
+}
+
+function DarkModeOption({isVisible, hideOption}: DarkModeOptionProps) {
+  const systemDefault = useColorScheme();
+  const {theme, isSystem, setMode, setSystem} =useThemeStorage();
+
+  const handlePressLight = () => {
+    setMode('light');
+    setSystem(false);
+    hideOption();
+  }
+
+  const handlePressDark = () => {
+    setMode('dark');
+    setSystem(false);
+    hideOption();
+  }
+
+  const handlePressSystem = () => {
+    setMode(systemDefault ?? 'light');
+    setSystem(true);
+    hideOption();
+  }
+
+  return (
+    <CompoundOption isVisible={isVisible} hideOption={hideOption}>
+      <CompoundOption.Background>
+        <CompoundOption.Container>
+          <CompoundOption.Button onPress={handlePressLight}>라이트 모드</CompoundOption.Button>
+          <CompoundOption.Divider/>
+          <CompoundOption.Button onPress={handlePressDark}>다크 모드</CompoundOption.Button>
+          <CompoundOption.Divider/>
+          <CompoundOption.Button onPress={handlePressSystem}>시스템 기본값 모드</CompoundOption.Button>
+        </CompoundOption.Container>
+        <CompoundOption.Container>
+          <CompoundOption.Button onPress={hideOption}>취소</CompoundOption.Button>
+        </CompoundOption.Container>
+      </CompoundOption.Background>
+    </CompoundOption>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default DarkModeOption;
