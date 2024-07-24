e09522ec5b227f50dd61b2d542d6285d3be9a7ab
rlacodud <rlacodud0313@naver.com>
2024-07-24 20:39:39 +0900

    update: colors light/dark 구분 적용

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,4 +1,4 @@
-const colors = {
+const common = {
   PINK_200: '#FAE2E9',
   PINK_400: '#EC87A5',
   PINK_500: '#BF5C79',
@@ -11,21 +11,39 @@ const colors = {
   YELLOW_400: '#FFE594',
   YELLOW_500: '#FACC15',
   PURPLE_400: '#C4C4E7',
-  WHITE: '#fff',
-  GRAY_100: '#F8F8F8',
-  GRAY_200: '#E7E7E7',
-  GRAY_300: '#D8D8D8',
-  GRAY_500: '#8E8E8E',
-  GRAY_700: '#575757',
-  BLACK: '#000',
+  UNCHANGED_WHITE: '#fff',
+  UNCHANGE_BLACK: '#000',
 };
 
+const colors = {
+  light: {
+    WHITE: '#fff',
+    GRAY_100: '#F8F8F8',
+    GRAY_200: '#E7E7E7',
+    GRAY_300: '#D8D8D8',
+    GRAY_500: '#8E8E8E',
+    GRAY_700: '#575757',
+    BLACK: '#161616',
+    ...common
+  },
+  dark: {
+    WHITE: '#161616',
+    GRAY_100: '#202124',
+    GRAY_200: '#3C4043',
+    GRAY_300: '#5E5E5E',
+    GRAY_500: '#8E8E8E',
+    GRAY_700: '#F8F8F8',
+    BLACK: '#fff',
+    ...common
+  },
+} as const;
+
 const colorHex = {
-  RED: colors.PINK_400,
-  BLUE: colors.BLUE_400,
-  GREEN: colors.GREEN_400,
-  YELLOW: colors.YELLOW_400,
-  PURPLE: colors.PURPLE_400,
+  RED: colors['light'].PINK_400,
+  BLUE: colors['light'].BLUE_400,
+  GREEN: colors['light'].GREEN_400,
+  YELLOW: colors['light'].YELLOW_400,
+  PURPLE: colors['light'].PURPLE_400,
 } as const;
 
 export {colors, colorHex};
