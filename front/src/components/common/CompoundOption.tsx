7bfe238e512155abbc8a7810e30cc3fc72c3d8c8
rlacodud <rlacodud0313@naver.com>
2024-08-01 21:07:29 +0900

    feat: CompoundOption CheckBox, Filter 추가

--- front/src/components/common/CompoundOption.tsx
+++ front/src/components/common/CompoundOption.tsx
@@ -1,9 +1,10 @@
 import { colors } from "@/constants";
 import useThemeStore from "@/store/useThemeStore";
 import { ThemeMode } from "@/types/common";
-import { Children, PropsWithChildren, ReactNode, createContext, useContext } from "react";
+import { PropsWithChildren, ReactNode, createContext, useContext } from "react";
 import { GestureResponderEvent, Modal, ModalProps, Pressable, PressableProps, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 interface OptionMainProps extends ModalProps {
   children: ReactNode;
@@ -122,11 +123,38 @@ function CheckBox({children, icon, isChecked = false, ...props}: CheckBoxProps) 
   return (
     <Pressable
       style={({pressed}) => [
-        pressed && styles.optionButtonPressed
+        pressed && styles.optionButtonPressed,
+        styles.checkBoxContainer
       ]}
       {...props}
     >
+      <Ionicons
+        name={`checkmark-circle${isChecked ? '' : '-outline'}`}
+        size={22}
+        color={colors[theme].BLUE_500}
+      />
+      {icon}
+      <Text style={styles.checkBoxText}>{children}</Text>
+    </Pressable>
+  )
+}
 
+interface FilterProps extends PressableProps {
+  children: ReactNode;
+  isSelected?: boolean;
+}
+
+function Filter({children, isSelected, ...props}: FilterProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable style={styles.filterContainer} {...props}>
+      <Text style={[isSelected ? styles.filterSelectedText : styles.filterText]}>{children}</Text>
+      <MaterialIcons
+        name="keyboard-arrow-down"
+        size={22}
+        color={isSelected ? colors[theme].BLUE_500 : colors[theme].GRAY_300}/>
     </Pressable>
   )
 }
@@ -137,6 +165,8 @@ export const CompoundOption = Object.assign(OptionMain, {
   Button,
   Title,
   Divider,
+  CheckBox,
+  Filter,
 });
 
 const styling = (theme: ThemeMode) => StyleSheet.create({
@@ -182,5 +212,32 @@ const styling = (theme: ThemeMode) => StyleSheet.create({
   border: {
     borderBottomColor: colors[theme].GRAY_200,
     borderBottomWidth: 1,
+  },
+  checkBoxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    gap: 10,
+  },
+  checkBoxText: {
+    color: colors[theme].BLACK,
+    fontSize: 15,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    gap: 5,
+  },
+  filterText: {
+    color: colors[theme].GRAY_300,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  filterSelectedText: {
+    color: colors[theme].BLUE_500,
+    fontSize: 15,
+    fontWeight: '500',
   }
 })
