e3f048704b28ddd41cbdedd3e3f58d81c5e8257d
rlacodud <rlacodud0313@naver.com>
2024-08-05 22:13:29 +0900

    update: useMarkerFilter 초기 세팅

--- front/src/hooks/useMarkerFilter.ts
+++ front/src/hooks/useMarkerFilter.ts
@@ -1,5 +1,24 @@
-function useMarkerFilter() {
+import { useState } from "react";
 
+const initialFilters = {
+  RED: true,
+  YELLOW: true,
+  GREEN: true,
+  BLUE: true,
+  PURPLE: true,
+  '1': true,
+  '2': true,
+  '3': true,
+  '4': true,
+  '5': true,
+}
+
+function useMarkerFilter() {
+  const [filterItems, setFilterItems] = useState(initialFilters);
+
+  const transfromFilterMarker = () => {
+    
+  }
 }
 
 export default useMarkerFilter;
