40545d86fcf67b3f0d9c928081b6398ac773ec6a
rlacodud <rlacodud0313@naver.com>
2024-08-11 17:30:48 +0900

    update: RootNavigator splash 화면 적용

--- front/src/navigations/root/RootNavigator.tsx
+++ front/src/navigations/root/RootNavigator.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
 import AuthStackNavigator from '@/navigations/stack/AuthStackNavigator';
 import MainDrawerNavigator from '@/navigations/drawer/MainDrawerNavigator';
 import useAuth from '@/hooks/queries/useAuth';
 import RetryErrorBoundary from '@/components/common/RetryErrorBoundary';
+import SplashScreen from 'react-native-splash-screen';
 
 function RootNavigator() {
-  const {isLogin} = useAuth();
+  const {isLogin, isLoginLoading} = useAuth();
+
+  useEffect(() => {
+    if(!isLoginLoading) {
+      setTimeout(() => {
+        SplashScreen.hide();        
+      }, 500);
+    }
+  }, [isLoginLoading]);
 
   return (
     <RetryErrorBoundary>
