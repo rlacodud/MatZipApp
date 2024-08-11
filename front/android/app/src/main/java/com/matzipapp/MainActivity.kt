583bd3099f177d7233b4211ea2f63e62683c842f
rlacodud <rlacodud0313@naver.com>
2024-08-11 17:36:35 +0900

    build: android splash 설정

--- front/android/app/src/main/java/com/matzipapp/MainActivity.kt
+++ front/android/app/src/main/java/com/matzipapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
+import com.cboy.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
 
@@ -22,6 +24,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null)
   }
 }
